4eceb54df56e3e4d35a8cf09965415b939893520
Omkar mali <omali.mali@thinkitive.com>
2024-04-02 12:37:07 +0530

    Commit For Section 12 Code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { useState , Fragment} from 'react';
+import React, { useState, useEffect } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
+  //using this we can set or store the user creaditials in localstorage i write in app.js folder not in loginhandler
+  // coz every time app.js will rerun again and again
+
+  // const storeUserLoggedInInformation = localStorage("isLoggedIn", "1");
+
+  // this code execute every component whenever components loads the state will change
+  //  if(storeUserLoggedInInformation=='1'){
+  //   setIsLoggedIn(true) // setting the state of loging page.
+  //  }
+
+
+  // i used the useEffect hook for setting the state and it will execute once and after w
+  //henever the dependencies updates it will again update so updation is depends on changing dependencies.
+
+  // useEffect(() => {
+  //   if (storeUserLoggedInInformation === '1') {
+  //     setIsLoggedIn(true); // setting the state of loging page.
+  //   }
+  // }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
   };
 
   return (
-    // we can use Fragment at the place of Div it is nothing but component which has no JSX code inside that 
-    // or we can srite <> </> tag only to write Fragment but it depends on our project env
-    
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </Fragment>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
